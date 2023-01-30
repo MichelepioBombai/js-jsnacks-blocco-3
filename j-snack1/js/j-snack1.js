961a4c557503d4979662ca50b0953b1d8cf73d46
MichelepioBombai <michelebombai@icloud.com>
2023-01-30 17:12:33 +0100

    first-commit

---
+++ j-snack1/js/j-snack1.js
@@ -0,0 +1,8 @@
+// Chiedi all'utente un valore minimo ed un valore massimo. Genera un numero casuale all'interno del range fornito dall'utente.
+
+const valoreMinimo = parseInt(prompt ("inserisci un valore minimo"))
+const valoreMassimo = parseInt(prompt("inserisci un valore massimo"))
+
+const numeroScelto = Math.floor(math.random(valoreMinimo) * valoreMassimo) ;
+
+ console.log(numeroScelto);
